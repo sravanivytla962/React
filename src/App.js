84aaa02bc56475a85f09cd9a18e53e26c0bf9674
Sravani <sravani.vytla@gmail.com>
2020-09-03 11:43:34 +0530

    RentalDetails and RentalListing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import './App.css';
 import {Header} from './shared/Header';
-import RentalList from './components/rental/RentalList';
-import { RentalDetail } from './components/rental/RentalDetail';
+import RentalListing from './components/rental/rental-listing/RentalListing';
+import RentalDetail from './components/rental/rental-detail/RentalDetail';
+
+
+
 const store = require('./reducers').init();
 class App extends Component {
   cons
@@ -15,9 +18,9 @@ class App extends Component {
       <div className="App">
         <Header></Header>
         <div className='container'>
-        <Route exact path="/" render={()=><Redirect to ='/rentals'/>}/>
-          <Route exact path="/rentals" component={RentalList}/>
-          <Route exact path="/rentals/:id" component={RentalDetail}/>  
+        <Route exact path='/' render={()=><Redirect to ='/rentals'/>}/>
+          <Route exact path='/rentals' component={RentalListing}/>
+          <Route exact path='/rentals/:id' component={RentalDetail}/>  
         </div>
       </div>
       </BrowserRouter>
--- src/redux/index.js
+++
@@ -1,9 +0,0 @@
-import * as redux from 'redux';
-import {rentalReducer} from './rental-reducer';
-export const init = () =>{
-    const reducer = redux.combineReducers({
-        rentals:rentalsReducer
-    })
-    const store = redux.createStore();
-    return store;
-}
